b9569beeebaaf2b86a1a2be106c849a0155f9c52
josiahnaguar11 <73671305+josiahnaguar11@users.noreply.github.com>
2025-08-05 05:42:25 +0100

    setup

---
+++ Forge/Forge/ForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeApp.swift
+//  Forge
+//
+//  Created by Josiah Naguar on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Forge/ForgeTests/ForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeTests.swift
+//  ForgeTests
+//
+//  Created by Josiah Naguar on 05/08/2025.
+//
+
+import Testing
+@testable import Forge
+
+struct ForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
